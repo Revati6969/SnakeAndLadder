3c2b5e8b29852554e731f398f8c59a0abfd331a9
Revati Tekale <revitekale1910@gmail.com>
2020-02-28 22:52:11 +0530

    ADDED:Play till position becomes 100

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -6,17 +6,25 @@ LADDER=1
 SNAKE=2
 position=0
 
-die=$((RANDOM%6+1))
-echo $die
-
-random=$((RANDOM%3))
-echo $random
-
-case $random in
-   $NUMBER)
-      position=0 ;;
-   $LADDER)
-      position=$(($position + $die)) ;;
-   $SNAKE)
-      position=$(($position - $die)) ;;
-esac
+function play()
+{
+   while [[ $position -lt 100 ]]
+   do
+      die=$((RANDOM%6+1))
+      random=$((RANDOM%3))
+      case $random in
+         $NUMBER)
+            position=$(($position + $NUMBER));;
+         $LADDER)
+            position=$(($position + $die)) ;;
+         $SNAKE)
+            position=$(($position - $die)) ;;
+      esac
+      if [[ $position -lt 0 ]]
+      then
+         position=0
+      fi
+    done
+}
+play
+echo "Position: $position"
