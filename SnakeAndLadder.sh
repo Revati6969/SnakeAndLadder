58e26dce3d58df13ecbaa36471efa8b6fd0c24a7
Revati Tekale <revitekale1910@gmail.com>
2020-02-28 22:25:33 +0530

    ADDED:Checked for option NoPlay,Ladder,Snake

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,7 +1,22 @@
 #!/bin/bash -x
 
 PLAYER=1
+NUMBER=0
+LADDER=1
+SNAKE=2
 position=0
 
 die=$((RANDOM%6+1))
 echo $die
+
+random=$((RANDOM%3))
+echo $random
+
+case $random in
+   $NUMBER)
+      position=0 ;;
+   $LADDER)
+      position=$(($position + $die)) ;;
+   $SNAKE)
+      position=$(($position - $die)) ;;
+esac
