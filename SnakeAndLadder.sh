925dba17946cec62f911eed27778fdb8f57d7723
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-02-29 11:56:39 +0530

    ADDED:Played with two players

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,45 +1,79 @@
 #!/bin/bash -x
 
-PLAYER=1
+PLAYER=2
 NUMBER=0
 LADDER=1
 SNAKE=2
-position=0
+position1=0
+position2=0
 diceCount=0
 
 function play()
 {
-   while [[ $position -lt 100 ]]
-   do
-      die=$((RANDOM%6+1))
-      diceCount=$(($diceCount + $die))
-      random=$((RANDOM%3))
-      case $random in
-         $NUMBER)
-            position=$(($position + $NUMBER));;
-         $LADDER)
-            position=$(($position + $die)) ;;
-         $SNAKE)
-            position=$(($position - $die)) ;;
-      esac
-      echo $position
-      if [[ $position -gt 100 ]]
-      then
-         position=$(($position-$die))
-      fi 
+    while [[ $position1 -lt 100 && $position2 -lt 100 ]]
+    do
+       for (( counter=1; counter<=$PLAYER; counter++ ))
+       do
+         if [[ $counter -eq 1 ]]
+         then
+            die=$((RANDOM%6+1))
+            random=$((RANDOM%3))
+            case $random in
+               $NUMBER)
+                  position1=$(($position1 + $NUMBER));;
+               $LADDER)
+                  position1=$(($position1 + $die)) ;;
+               $SNAKE)
+                  position1=$(($position1 - $die)) ;;
+            esac
+            if [[ $position1 -gt 100 ]]
+            then
+               position1=$(($position1-$die))
+            fi 
       
-      if [[ $position -eq 100 ]]
-      then
-         echo "You won"
-         break
-      fi
+            if [[ $position1 -eq 100 ]]
+            then
+               echo "Player $counter won"
+               break
+            fi
 
-      if [[ $position -lt 0 ]]
-      then
-         position=0
-      fi
-    done
+            if [[ $position1 -lt 0 ]]
+            then
+               position1=0
+            fi
+         fi
+       
+         if [[ $counter -eq 2 ]]
+         then
+            die=$((RANDOM%6+1))
+            random=$((RANDOM%3))
+            case $random in
+               $NUMBER)
+                  position2=$(($position2 + $NUMBER));;
+               $LADDER)
+                  position2=$(($position2 + $die)) ;;
+               $SNAKE)
+                  position2=$(($position2 - $die)) ;;
+            esac
+ 
+            if [[ $position2 -gt 100 ]]
+            then
+               position2=$(($position2-$die))
+            fi 
+
+            if [[ $position2 -eq 100 ]]
+            then
+               echo "Player $counter won"
+               break
+            fi
+
+            if [[ $position2 -lt 0 ]]
+            then
+               position2=0
+            fi
+         fi
+
+      done
+   done
 }
-play
-echo "Winner At: $position"
-echo "diceCount: $diceCount"
+play 
