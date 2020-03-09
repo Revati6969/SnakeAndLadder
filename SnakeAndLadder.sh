8e3b648fbb6edc94cea194130d58c58be9136c81
Revati6969 <61040382+Revati6969@users.noreply.github.com>
2020-03-09 10:09:49 +0530

    MODIFIED:Added comments

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -x
-
+#CONSTANTS
 PLAYER=2
 NUMBER=0
 LADDER=1
 SNAKE=2
+#VARIABLES
 position1=0
 position2=0
 diceCount=0
-
+#PLAY GAME
 function play()
 {
+    #PLAY TILL WINNING POSITION REACHES
     while [[ $position1 -lt 100 && $position2 -lt 100 ]]
     do
        for (( counter=1; counter<=$PLAYER; counter++ ))
        do
+         #PLAYER 1 PLAYS
          if [[ $counter -eq 1 ]]
          then
             die=$((RANDOM%6+1))
@@ -42,7 +45,7 @@ function play()
                position1=0
             fi
          fi
-       
+         #PLAYER 2 PLAYS
          if [[ $counter -eq 2 ]]
          then
             die=$((RANDOM%6+1))
